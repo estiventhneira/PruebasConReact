8807a89e174862dbb9fd31f910dcaa6f73263db6
estiventhneira <estiventhneira@gmail.com>
2020-08-14 00:42:51 -0500

    agregando botones

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-export default function Button(props) {
-  const { text } = props;
-  return (
-    <div>
-      <button type="button">{text}</button>
-    </div>
-  );
+class Button extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  handleClick = () => {
+    console.log("hola");
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Manzanas: {count}</h1>
+        <button onClick={this.handleClick} type="button">
+          Click
+        </button>
+      </div>
+    );
+  }
 }
+
+export default Button;
